23a43ec719aa89569493b4f4e90052ade5d9ac65
Derpmaster45 <31549635+Derpmaster45@users.noreply.github.com>
2021-12-09 20:18:04 -0600

    Started working on story

--- MazeEscape/MazeEscape/Program.cs
+++ MazeEscape/MazeEscape/Program.cs
@@ -6,7 +6,25 @@ namespace MazeEscape
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("You Wake up, and you don't know where you are.");
+            // intro and first choice
+            Console.WriteLine("You wake up, and you don't know where you are. There are 4 pathways. You can:\n" +
+            	"Go North\n" +
+            	"Go South\n" +
+            	"Go East\n" +
+            	"Go West");
+            Console.WriteLine("What will you choose?");
+            string option = Console.ReadLine();
+            // use a switch statement to bulkify the statements
+            switch (option)
+            {
+                case "Go North":
+                case "go north":
+                case "GO NORTH":
+                    break;
+                default:
+                    Console.WriteLine("");
+                    break;
+            }
         }
     }
 }
