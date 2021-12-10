b9b1c1c22e8fb0692f664103fc551bea07e9695c
Derpmaster45 <31549635+Derpmaster45@users.noreply.github.com>
2021-12-09 18:44:54 -0600

    Inital Commit of game files

---
+++ MazeEscape/MazeEscape/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MazeEscape
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("You Wake up, and you don't know where you are.");
+        }
+    }
+}
