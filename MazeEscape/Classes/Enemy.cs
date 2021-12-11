9521e785961973dddf822951879a9da2fa964213
Derpmaster45 <31549635+Derpmaster45@users.noreply.github.com>
2021-12-11 14:43:54 -0600

    Added enemy class and enum

---
+++ MazeEscape/Classes/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MazeEscape.Classes
+{
+    public class Enemy
+    {
+        public string name { get; set; }
+        public double attack { get; set; }
+        public double defense { get; set; }
+
+    }
+}
+
