191ab25de55c4bce7f67498acded3b4464d6fb0d
Derpmaster45 <31549635+Derpmaster45@users.noreply.github.com>
2021-12-26 09:12:57 -0600

    added battle method with no real functionality

--- MazeEscape/MazeEscape/ExperimentalClasses/BattleSystem.cs
+++ MazeEscape/MazeEscape/ExperimentalClasses/BattleSystem.cs
@@ -10,13 +10,14 @@ namespace MazeEscape.ExperimentalClasses
         PlayerStats player = new PlayerStats();
 
         // battlesystem fuction
-        public void battleStart(EnemyTypes type)
+        public void battleStart(EnemyTypes type, PlayerStats player)
         {
+            // introduce the battle
+            Console.WriteLine($"You encounter a {type.ToString().ToLower()}, what would you like to do?");
+            // show the menu!
             while (target.health > 0)
             {
-                // introduce the battle
-                Console.WriteLine($"You encounter a {type.ToString().ToLower()}, what would you like to do?");
-                // show the menu!
+                
                 Console.WriteLine("Menu:\n 1: Physical Attack\n 2: Magic Attack\n 3: Defend\n 4: Retreat");
                 // set action to 0 to keep from defaulting to an action.
                 int action = 0;
