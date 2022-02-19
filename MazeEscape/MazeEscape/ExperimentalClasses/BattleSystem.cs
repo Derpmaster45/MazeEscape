95af91587fd85682a1c45a6df66b59a98d01e796
Derpmaster45 <31549635+Derpmaster45@users.noreply.github.com>
2022-02-19 10:26:46 -0600

    Continued working on the north path

--- MazeEscape/MazeEscape/ExperimentalClasses/BattleSystem.cs
+++ MazeEscape/MazeEscape/ExperimentalClasses/BattleSystem.cs
@@ -8,24 +8,23 @@ namespace MazeEscape.ExperimentalClasses
         // object declaration
         public Enemy target = new Enemy();
         PlayerStats player = new PlayerStats();
+        private WeaponsEnum weapons { get; set; }
 
         // battlesystem fuction
         public void battleStart(EnemyTypes type)
         {
-            // introduce the battle
-            Console.WriteLine($"You encounter a {type.ToString().ToLower()}, what would you like to do?");
-            // show the menu!
-            Console.WriteLine("Menu:\n 1: Physical Attack\n 2: Magic Attack\n 3: Defend\n 4: Retreat");
             while (target.health > 0)
             {
-               
+                // introduce the battle
+                Console.WriteLine($"You encounter a {type.ToString().ToLower()}, what would you like to do?");
+                // show the menu!
+                Console.WriteLine("Menu:\n 1: Physical Attack\n 2: Magic Attack\n 3: Defend\n 4: Retreat");
                 // set action to 0 to keep from defaulting to an action.
                 int action = 0;
                 int.TryParse(Console.ReadLine(), out action);
                 switch (action)
                 {
                     case 1:
-                        // insert action code here
                         break;
                     case 2:
                         // insert action code here
@@ -37,12 +36,6 @@ namespace MazeEscape.ExperimentalClasses
                         // insert action code here
                         break;
 
-                }
-                if (player.health <=0) 
-                {
-                    Console.WriteLine("Game Over\n press any key to exit");
-                    Console.ReadKey();
-                    System.Environment.Exit(0);
                 }
             }
 
--- MazeEscape/MazeEscape/Program.cs
+++ MazeEscape/MazeEscape/Program.cs
@@ -12,15 +12,17 @@ namespace MazeEscape
             // object instance creation
             WeaponsEnum weaponOfChoice= new WeaponsEnum();
             WeaponSTATS stats = new WeaponSTATS();
+            
             //PlayerStats pStats =();
 
             // stats setup
-           // pStats.setHealth = 100;
+            // pStats.setHealth = 100;
             double weaponDamage;
 
             // intro and first choice
+            #region 
             Console.WriteLine("You wake up, and you don't know where you are. There are 4 pathways. You can:\n" +
-            	"Go North\n" +
+            "Go North\n" +
             	"Go South\n" +
             	"Go East\n" +
             	"Go West");
@@ -68,10 +70,40 @@ namespace MazeEscape
                     Console.WriteLine("You head down the path that leads south.\n On that path you find a slingshot do you pick it up?");
                     break;
                 default:
-                    Console.WriteLine("There are 4 pathways. You can:\n\" +\n            \t\"Go North\n\" +\n            \t\"Go South\n\" +\n            \t\"Go East\n\" +\n          " +
-                    	"\t\"Go West\"");
+                    Console.WriteLine("Command not recognised");
                     break;
+                    #endregion
+            }
+            if (option.ToLower()=="go north") 
+            {
+                if (weaponOfChoice == WeaponsEnum.SHORTSWORD) 
+                {
+                    Console.WriteLine("You come across a thicket it looks peaceful, but somewhat hard to walk through. What would you like to do?");
+                    string actionChoice = Console.ReadLine();
+                    switch (actionChoice)
+                    {
+                        case "Cut down thicket":
+                        case "cut down thicket":
+                            Console.WriteLine("You took a swing at the thicket, and successfully cutting some of the thicket. \n" +
+                                "Do you continue cutting the thicket?");
+                            actionChoice = Console.ReadLine().ToLower();
+                            int actionCount = 0;
+                            while (actionChoice.ToLower()=="yes" || actionChoice.ToLower() == "y"&&actionCount<=5) 
+                            {
+                                Console.Write("You continue to cut the thicket, you hear a roar that gets louder as you continue to cut.\n" +
+                                    "Would you like to continue cutting the thicket?");
+                                actionChoice = Console.ReadLine().ToLower();
+                               
+                             }
+
+                            break;
+
+                            
+                    }
+
+                }
             }
         }
+        
     }
 }
