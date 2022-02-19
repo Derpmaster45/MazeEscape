7853dd7df766d2b1c0d4031766a9f8e8071eb5a8
Derpmaster45 <31549635+Derpmaster45@users.noreply.github.com>
2022-02-19 14:10:19 -0600

    Added functions for the retreat and defend actions
    these are bare bone functions at the moment as I will be working on writing the functions later this week

--- MazeEscape/MazeEscape/MazeEscape/ExperimentalClasses/BattleSystem.cs
+++ MazeEscape/MazeEscape/MazeEscape/ExperimentalClasses/BattleSystem.cs
@@ -13,32 +13,41 @@ namespace MazeEscape.ExperimentalClasses
         // battlesystem fuction
         public void battleStart(EnemyTypes type)
         {
-            while (target.health > 0)
+            while (target.health > 0|| player.health>0)
             {
                 // introduce the battle
                 Console.WriteLine($"You encounter a {type.ToString().ToLower()}, what would you like to do?");
                 // show the menu!
-                Console.WriteLine("Menu:\n 1: Physical Attack\n 2: Magic Attack\n 3: Defend\n 4: Retreat");
+                Console.WriteLine("Menu:\n 1: Physical Attack\n 2: Defend\n 3: Retreat");
                 // set action to 0 to keep from defaulting to an action.
                 int action = 0;
                 int.TryParse(Console.ReadLine(), out action);
                 switch (action)
                 {
                     case 1:
+                        // create a get weapon type and deal damage from there
                         break;
                     case 2:
                         // insert action code here
                         break;
                     case 3:
-                        // insert action code here
+                        // do math to figure out if player was sucessful in retreating
+                        Console.WriteLine("You retreated");
                         break;
-                    case 4:
-                        // insert action code here
-                        break;
+                   
 
                 }
             }
 
         }
+        public void retreat() 
+        {
+           // if(player.speed<target.)
+         }
+        public void defendPlayer() 
+        {
+         
+         }
     }
+
 }
